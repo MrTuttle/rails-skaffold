4146b53eb36d85fc7e0458d5c0d9c6775c8b471e
Florent <florent.vincerot@me.com>
2023-04-03 15:27:57 +0200

    rails new + scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,6 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
+# incrementally modify your database, and then regenerate this schema definition.Flat
 #
 # This file is the source Rails uses to define your schema when running `bin/rails
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_165546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+puts "creating flats..."
+Flat.create!(name: "Poppy Puppy", address: "12 rue Gambetta, 71000 Mâcon")
+Flat.create!(name: "La maison du coin", address: "12 rue Impecable, 71000 Mâcon")
+Flat.create!(name: "Yummy", address: "12 rue m, 71000 Mâcon")
+puts "ok"
